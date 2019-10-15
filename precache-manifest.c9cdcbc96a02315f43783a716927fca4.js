6a5987c7b0ebeb79897cb6d4a553840b2cb802ec
whtalx <whtalx@gmail.com>
2019-10-15 08:46:41 +0300

    Updates

---
+++ precache-manifest.c9cdcbc96a02315f43783a716927fca4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f2627fc6066739080657e9c2f58db11",
+    "url": "/sli/index.html"
+  },
+  {
+    "revision": "edcf2a93fcf3400d0fb9",
+    "url": "/sli/static/css/main.c12a1b84.chunk.css"
+  },
+  {
+    "revision": "b6b5d645dd20a3c67d83",
+    "url": "/sli/static/js/2.7f3ca6fe.chunk.js"
+  },
+  {
+    "revision": "edcf2a93fcf3400d0fb9",
+    "url": "/sli/static/js/main.b1aac02d.chunk.js"
+  },
+  {
+    "revision": "76c42e82c5f1014884da",
+    "url": "/sli/static/js/runtime-main.b4bd5b79.js"
+  }
+]);
