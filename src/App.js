0122ab8e1fe0018360642ba79c409d61ece78bd6
whtalx <whtalx@gmail.com>
2019-10-16 21:39:53 +0300

    sli 2

--- src/App.js
+++ src/App.js
@@ -1,77 +1,46 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-
-const Card = ({ index, color }) =>
-  <div className="card" id={ index } style={{ backgroundColor: color }}>{ index }</div>
-
-
-export default class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      colors: ['antiquewhite', 'azure', 'beige', 'cornsilk', 'lavender', 'lightgoldenrodyellow'],
-      offset: 0,
-    };
-    this.state.cards = [this.state.colors.length - 1, 0, 1];
-    this.handleMouseDown = (event) => {
-      const target = event.target.id === 'slider'
-        ? event.target
-        : event.target.parentElement.id === 'slider' && event.target.parentElement;
-    
-      if (!target) { return }
-      const startX = event.clientX;
-      let modified = false;
-      let shift = 0;
-      const move = (event) => {
-        const translate = event.clientX - startX + shift;
-        if (Math.abs(translate) < 180) {
-          this.setState({ offset: translate });
-          return;
-        }
-        if (!modified) {
-          modified = true;
-          if (translate < 0) {
-            shift = 360;
-            this.setState(state => {
-              state.cards.shift();
-              state.cards.push(state.cards[1] + 1 < state.colors.length ? state.cards[1] + 1 : 0);
-              state.offset = translate;
-              return state;
-            });
-          } else {
-            shift = -360;
-            this.setState(state => {
-              state.cards.pop();
-              state.cards.unshift(state.cards[0] - 1 >= 0 ? state.cards[0] - 1 : state.colors.length - 1);
-              state.offset = translate;
-              return state;
-            });
-          }
-        }
-      }
-    
-      document.addEventListener('mousemove', move);
-      document.addEventListener('mouseup', () => {
-        document.removeEventListener('mousemove', move);
-        target.style.transform = '';
-      }, { once: true });
-    }
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <div id="slider-wrapper">
-          <div id="slider" onMouseDown={ this.handleMouseDown } style={{transform: `translateX(${ this.state.offset }px)`}}>
-            {
-              this.state.cards.map((item) =>
-                <Card index={ item } color={this.state.colors[item]} key={this.state.colors[item]} />
-              )
-            }
-          </div>
+export default () =>
+  <div>
+    <input hidden type="radio" name="radio" id="one" defaultChecked />
+    <input hidden type="radio" name="radio" id="two" />
+    <input hidden type="radio" name="radio" id="three" />
+    <input hidden type="radio" name="radio" id="four" />
+    <div className="controls">
+      <label htmlFor="one"></label>
+      <label htmlFor="two"></label>
+      <label htmlFor="three"></label>
+      <label htmlFor="four"></label>
+    </div>
+    <div className="slider">
+      <div className="container big">
+        <div className="slide">
+          <img src={ `https://picsum.photos/800/600?random=${Math.round(Math.random() * 80)}` } alt="big" className="img-big" width="800" />
+        </div>
+        <div className="slide">
+          <img src={ `https://picsum.photos/800/600?random=${Math.round(Math.random() * 80)}` } alt="big" className="img-big" width="800" />
+        </div>
+        <div className="slide">
+          <img src={ `https://picsum.photos/800/600?random=${Math.round(Math.random() * 80)}` } alt="big" className="img-big" width="800" />
+        </div>
+        <div className="slide">
+          <img src={ `https://picsum.photos/800/600?random=${Math.round(Math.random() * 80)}` } alt="big" className="img-big" width="800" />
         </div>
       </div>
-    );
-  }
-}
+      <div className="container smol">
+        <div className="slide">
+          <img src={ `https://picsum.photos/400/280?random=${Math.round(Math.random() * 80)}` } alt="smol" className="img-smol" width="400" />
+        </div>
+        <div className="slide">
+          <img src={ `https://picsum.photos/400/280?random=${Math.round(Math.random() * 80)}` } alt="smol" className="img-smol" width="400" />
+        </div>
+        <div className="slide">
+          <img src={ `https://picsum.photos/400/280?random=${Math.round(Math.random() * 80)}` } alt="smol" className="img-smol" width="400" />
+        </div>
+        <div className="slide">
+          <img src={ `https://picsum.photos/400/280?random=${Math.round(Math.random() * 80)}` } alt="smol" className="img-smol" width="400" />
+        </div>
+      </div>
+    </div>
+  </div>
